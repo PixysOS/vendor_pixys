27415bbdaa81f2d32a10ad39cc3f2cb1dd771d5c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-11 12:56:37 -0200

    Remove Lineage Bootanimation and Add pixel Bootanimation 1080: From walleye factory image 1440: From taimen factory image 720: From https://androidfilehost.com/?fid=745849072291684773

--- config/common.mk
+++ config/common.mk
@@ -80,6 +80,17 @@ ifeq ($(WITH_TWRP),true)
 include vendor/pixys/config/twrp.mk
 endif
 
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/pixys/media/bootanimation_720.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/pixys/media/bootanimation_1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/pixys/media/bootanimation_1440.zip:system/media/bootanimation.zip
+else
+    $(error "Bootanimation: TARGET_BOOT_ANIMATION_RES is undefined")
+endif
+
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
