eead46fac7f7cf6a9954f6c887e044c72104074e
Akhil Narang <akhilnarang.1999@gmail.com>
2017-12-05 13:17:05 +0530

    vendor: update gerrit remote to set username
     aex edits :-
      * Update logic to check for existing remote
      * To setup your username use
    
          git config --global gerrit.aospextended.com.username "Your Gerrit Username"
    
    Change-Id: I7af54b1366434faebbacc8fc017e63729c902d5f

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,10 +1,10 @@
 function __print_pixys_functions_help() {
 cat <<EOF
-Additional JDC functions:
+Additional PIXYS functions:
 - mka:             Builds using SCHED_BATCH on all processors.
 - gerrit:          Adds a remote for PixysOS Gerrit
 EOF
-}
+} 
 
 function mk_timer()
 {
@@ -49,13 +49,14 @@ function gerrit()
     if [ ! -d ".git" ]; then
         echo -e "Please run this inside a git directory";
     else
-        if [ -d ".git/refs/remotes/gerrit" ]; then
+        if [[ ! -z $(git config --get remote.gerrit.url) ]]; then
             git remote rm gerrit;
         fi
+        [[ -z "${GERRIT_USER}" ]] && export GERRIT_USER=$(git config --get gerrit.pixysos.org.username);
         if [[ -z "${GERRIT_USER}" ]]; then
-            git remote add gerrit $(git remote -v | grep PixysOS | awk '{print $2}' | uniq | sed -e 's|https://github.com/PixysOS|ssh://gerrit.pixysos.org:29418/PixysOS|');
+            git remote add gerrit $(git remote -v | grep PixysOS | awk '{print $2}' | uniq | sed -e "s|https://github.com/PixysOS|ssh://gerrit.pixysos.org:29418/PixysOS|");
         else
-            git remote add gerrit $(git remote -v | grep PixysOS | awk '{print $2}' | uniq | sed -e 's|https://github.com/PixysOS|ssh://${GERRIT_USER}@gerrit.pixysos.org:29418/PixysOS|');
+            git remote add gerrit $(git remote -v | grep PixysOS | awk '{print $2}' | uniq | sed -e "s|https://github.com/PixysOS|ssh://${GERRIT_USER}@gerrit.pixysos.org:29418/PixysOS|");
         fi
     fi
 }
@@ -63,7 +64,7 @@ function gerrit()
 function fixup_common_out_dir() {
     common_out_dir=$(get_build_var OUT_DIR)/target/common
     target_device=$(get_build_var TARGET_DEVICE)
-    if [ ! -z $JDC_FIXUP_COMMON_OUT ]; then
+    if [ ! -z $AOSP_FIXUP_COMMON_OUT ]; then
         if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
             mv ${common_out_dir} ${common_out_dir}-${target_device}
             ln -s ${common_out_dir}-${target_device} ${common_out_dir}
