0b32c86fd31f004f4787887a0d61472dd2d0d229
Subinsmani <subins.mani@gmail.com>
2018-08-05 21:51:17 +0530

    vendor: Call the Overlays Link

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -7,3 +7,6 @@ include vendor/pixys/themes/config.mk
 
 # Pixys props
 $(call inherit-product, vendor/pixys/config/pixys_props.mk)
+
+# Call Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pixys/themes/overlay/common
