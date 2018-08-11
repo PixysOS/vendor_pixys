42574aa2363b7556844ddf06d6ffcf9a60e35248
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-11 14:20:59 -0400

    NexusLauncherPrebuilt: Build with platform keys
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: FATAL EXCEPTION: main
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: Process: com.google.android.apps.nexuslauncher, PID: 23169
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: java.lang.NoSuchMethodError: No static method setContextPriority(I)V in class Landroid/view/ThreadedRenderer; or its super classes (declaration of 'android.view.ThreadedRenderer' appears in /system/framework/framework.jar!classes2.dex)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.systemui.shared.system.ThreadedRendererCompat.setContextPriority(SourceFile:31)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.quickstep.QuickstepProcessInitializer.init(SourceFile:33)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.google.android.apps.nexuslauncher.NexusProcessInitializer.init(SourceFile:16)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.launcher3.MainProcessInitializer.initialize(SourceFile:32)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.launcher3.LauncherProvider.onCreate(SourceFile:117)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.content.ContentProvider.attachInfo(ContentProvider.java:1917)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.content.ContentProvider.attachInfo(ContentProvider.java:1892)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread.installProvider(ActivityThread.java:6391)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread.installContentProviders(ActivityThread.java:5938)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread.handleBindApplication(ActivityThread.java:5853)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread.access$1100(ActivityThread.java:199)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1650)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:193)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:6669)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:495)
    08-11 13:20:39.103 23169 23169 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:858)
    
    Change-Id: I0e994b07b93798e7e6d49c112d91d371764d4ecc

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -13,7 +13,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := NexusLauncherPrebuilt
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := NexusLauncherPrebuilt/NexusLauncherPrebuilt.apk
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3 Launcher3QuickStep
