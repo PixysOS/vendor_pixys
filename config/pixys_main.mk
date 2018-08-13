8bdb373ce41a45ff4b679d6721d8ba337508dc8b
Subins Mani <subins.mani@gmail.com>
2018-08-13 07:08:09 +0530

    Build Only Launcher3 As of now.

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -1,15 +1,3 @@
 # PixysOS apps
 PRODUCT_PACKAGES += \
-    Launcher3 \
-    Stk \
-    Terminal \
-    PixysOTA
-
-# Pixys Theme
-include vendor/pixys/themes/config.mk
-
-# Pixys props
-$(call inherit-product, vendor/pixys/config/pixys_props.mk)
-
-# Call Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/pixys/themes/overlay/common
+    Launcher3
