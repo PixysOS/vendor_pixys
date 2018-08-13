8b8fdd99428201a7f9967a60e2419aa43dba6cf0
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-13 09:18:05 -0400

    Add support to screenshot editor(Markup)
    Change-Id: Id1289b70863673b3a6ccd3bf0aa97e68437ee187
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -1,5 +1,11 @@
 # PixysOS apps
 PRODUCT_PACKAGES += \
+     MarkupGoogle
+
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
+    vendor/pixys/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
 # Pixys Theme
 include vendor/pixys/themes/config.mk
