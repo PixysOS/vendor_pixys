269ddf7ba0c0b996c611ca2f3b2fa9c2c73daf46
Subinsmani <subins.mani@gmail.com>
2018-08-15 22:39:56 +0530

    Vendor: Lets Build NexusLauncherPrebuilt Instead of Luancher3

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -1,3 +1,9 @@
 # PixysOS apps
 PRODUCT_PACKAGES += \
-    Launcher3
+    Launcher3 \
+    NexusLauncherPrebuilt
+
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
+    vendor/pixys/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
