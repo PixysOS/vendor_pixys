e0a44baba348fe829e1bc82653201a1fe5daba69
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-18 12:16:53 +0000

    Add Pixel's sysconfigs
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- config/pixys_main.mk
+++ config/pixys_main.mk
@@ -7,6 +7,10 @@ PRODUCT_COPY_FILES += \
     vendor/pixys/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/pixys/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Pixys Theme
 include vendor/pixys/themes/config.mk
 
