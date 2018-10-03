ebd3114e8e4dcc6e59d1f898c6aa3e012d42277b
LuK1337 <priv.luk@gmail.com>
2018-10-03 19:01:05 +0000

    pixys: Add kernel source to product soong namespace
    Change-Id: Iace4732652a4e13eb473272227d07df808008d28
    Signed-off-by: Yash-Garg <ben10.yashgarg@gmail.com>

--- config/BoardConfigPixys.mk
+++ config/BoardConfigPixys.mk
@@ -2,3 +2,12 @@
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
     TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
 endif
+
+TARGET_AUTO_KDIR := $(shell echo $(TARGET_DEVICE_DIR) | sed -e 's/^device/kernel/g')
+TARGET_KERNEL_SOURCE ?= $(TARGET_AUTO_KDIR)
+KERNEL_SRC := $(TARGET_KERNEL_SOURCE)
+
+ifneq "$(wildcard $(KERNEL_SRC) )" ""
+PRODUCT_SOONG_NAMESPACES += \
+    $(KERNEL_SRC)
+endif
