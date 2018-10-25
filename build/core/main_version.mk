c456697d7ecb1f7d54ef3e06972ddcea5d197daf
Subinsmani <subins.mani@gmail.com>
2018-10-25 17:32:07 +0530

    [vendor] Fix Pixys buildtype
    Change-Id: I9063881183fc69a9332084f7379555e00e5f82a2

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,5 +1,5 @@
 # PixysOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.pixys.version_number=$(PIXYS_VERSION_NUMBER) \
-    ro.pixys.releasetype=$(PIXYS_BUILDTYPE) \
+    ro.pixys.releasetype=$(PIXYS_BUILD_TYPE) \
     ro.pixys.version=$(PIXYS_VERSION)
