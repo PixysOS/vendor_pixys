9ecbbc5e4a365505426c9ec52e58a79c592ca567
Subinsmani <subins.mani@gmail.com>
2018-10-26 00:17:07 +0530

    [vendor] Add Pixys Display Version to build/core/main_version
    Change-Id: I285995e67baa0806cd5e62544d4df0cf951126c7

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -2,4 +2,5 @@
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.pixys.version_number=$(PIXYS_VERSION_NUMBER) \
     ro.pixys.releasetype=$(PIXYS_BUILD_TYPE) \
-    ro.pixys.version=$(PIXYS_VERSION)
+    ro.pixys.version=$(PIXYS_VERSION) \
+    ro.pixys.display.version=$(PIXYS_DISPLAY_VERSION)
