cd7dc159adf4175dcc957bd994302e0ea9a35725
Amarjithtk <amarjithraveendran@gmail.com>
2018-11-01 13:34:44 +0200

    Added WallpaperPickerGooglePrebuilt Change-Id: I11f9891578430ae0becc6f2a488aed419ddc1ddb
    Added Google Wallpapers Picker
    
    Change-Id: I11f9891578430ae0becc6f2a488aed419ddc1ddb

--- config/common.mk
+++ config/common.mk
@@ -19,6 +19,13 @@ PIXYS_MOD_VERSION := $(PIXYS_VERSION)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    persist.sys.wfd.nohdcp=1 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
+
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.pixys.version=$(PIXYS_VERSION) \
   ro.pixys.releasetype=$(PIXYS_BUILD_TYPE) \
