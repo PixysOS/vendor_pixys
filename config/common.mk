AOSOP/vendor_aosop

Create /data/pixys_updates to store updates
GmsCore deletes the files in /data/ota_package/, including the files
downloaded by Updater. Create a new directory where Updater can store
its files not to have them disappear randomly.

config/common.mk
@@ -56,6 +56,10 @@ else
     ro.device.cache_dir=/cache
 endif
 
+# init file
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/etc/init.local.rc:system/etc/init/init.arrow.rc
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/pixys/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \

PixysOS/vendor_pixys

Change-Id: I99802f4d9eba21cbafea3c196cf4323d69558b37

config/common.mk
@@ -56,6 +56,10 @@ else
     ro.device.cache_dir=/cache
 endif
 
+# init file
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/etc/init.local.rc:system/etc/init/init.pixys.rc
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/pixys/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \