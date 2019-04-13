bdcd7b1d033d431496c4c1baebd2add465cd550a
Subinsmani <subins.mani@gmail.com>
2019-04-13 17:25:26 +0530

    vendor: Changes for PixysOS
    Change-Id: I59c23dac6621d0e8b3d1d640bb1366c4b838b32a

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -787,16 +787,16 @@ function oat2dex() {
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
 
-        export BAKSMALIJAR="$AOSP_ROOT"/vendor/aosp/build/tools/smali/baksmali.jar
-        export SMALIJAR="$AOSP_ROOT"/vendor/aosp/build/tools/smali/smali.jar
+        export BAKSMALIJAR="$AOSP_ROOT"/vendor/pixys/build/tools/smali/baksmali.jar
+        export SMALIJAR="$AOSP_ROOT"/vendor/pixys/build/tools/smali/smali.jar
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$AOSP_ROOT"/vendor/aosp/build/tools/"$HOST"/vdexExtractor
+        export VDEXEXTRACTOR="$AOSP_ROOT"/vendor/pixys/build/tools/"$HOST"/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$AOSP_ROOT"/vendor/aosp/build/tools/"$HOST"/compact_dex_converter
+        export CDEXCONVERTER="$AOSP_ROOT"/vendor/pixys/build/tools/"$HOST"/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
@@ -1037,7 +1037,7 @@ function extract() {
             # If OTA is block based, extract it.
             elif [ -a "$DUMPDIR"/system.new.dat ]; then
                 echo "Converting system.new.dat to system.img"
-                python "$AOSP_ROOT"/vendor/aosp/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
+                python "$AOSP_ROOT"/vendor/pixys/build/tools/sdat2img.py "$DUMPDIR"/system.transfer.list "$DUMPDIR"/system.new.dat "$DUMPDIR"/system.img 2>&1
                 rm -rf "$DUMPDIR"/system.new.dat "$DUMPDIR"/system
                 mkdir "$DUMPDIR"/system "$DUMPDIR"/tmp
                 echo "Requesting sudo access to mount the system.img"
