fd312eeccd8c0d00801e8502e3bcb884af72f983
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-16 15:58:42 -0300

    vendor: Add qcom platform type to soongs vars
    Required for new commits on frameworks/native
    
    * Fix lag during screen recording on some platforms
    * Fix screen flickering after disconnecting from Android Auto on some platforms
    
    Change-Id: I4872e9064df6515cde379da4da79dbc3b82a8829

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,3 +1,5 @@
+include vendor/pixys/config/BoardConfigQcom.mk
+
 add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
 _contents := $(_contents)    "Pixys":{$(newline)
@@ -6,22 +8,27 @@ _contents := $(_contents)    "Pixys":{$(newline)
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_str, Additional_gralloc_10_usage_bits,       $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
-$(call add_json_bool, Device_support_hwfde, $(filter true,   $(TARGET_HW_DISK_ENCRYPTION)))
-$(call add_json_bool, Device_support_hwfde_perf,             $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
-$(call add_json_bool, Device_support_legacy_hwfde,           $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
-$(call add_json_bool, Device_support_wait_for_qsee,          $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
-$(call add_json_val_default, Bootloader_message_offset,      $(BOOTLOADER_MESSAGE_OFFSET), 0)
-$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
-$(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
-$(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
-$(call add_json_str, Target_destroyed_mutex_usage_whitelist, $(TARGET_DESTROYED_MUTEX_USAGE_WHITELIST))
-$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
-$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
-$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
-$(call add_json_bool, Uses_nvidia_enhancements,              $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
-$(call add_json_bool, Uses_qcom_bsp_legacy,                  $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
-$(call add_json_bool, Uses_qti_camera_device,                $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_str, Additional_gralloc_10_usage_bits,                   $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_bool, Device_support_hwfde, $(filter true,               $(TARGET_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_hwfde_perf,                         $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
+$(call add_json_bool, Device_support_legacy_hwfde,                       $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
+$(call add_json_bool, Device_support_wait_for_qsee,                      $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
+$(call add_json_val_default, Bootloader_message_offset,                  $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_bool, Has_legacy_camera_hal1,                            $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Needs_text_relocations,                            $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str,  Specific_camera_parameter_library,                 $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str, Target_destroyed_mutex_usage_whitelist,             $(TARGET_DESTROYED_MUTEX_USAGE_WHITELIST))
+$(call add_json_str_omitempty, Target_process_sdk_version_override,      $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str,  Target_shim_libs,                                  $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Uses_generic_camera_parameter_library,             $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool, Uses_nvidia_enhancements,                          $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy,                              $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qti_camera_device,                            $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Uses_qcom_um_family,                               $(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_3_18_family,                          $(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_4_family,                           $(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_9_family,                           $(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_14_family,                          $(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
