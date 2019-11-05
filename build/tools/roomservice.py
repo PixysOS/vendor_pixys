7170a042e4babc1939e080c4edbab986ae7a4ca8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-05 20:18:29 +0000

    vendor: roomservice: Add support for custom branch
    Change-Id: Iacb7b2eeb49c5809952e070755418cff9ef0a9e6

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -45,6 +45,7 @@ default_manifest = ".repo/manifest.xml"
 
 custom_local_manifest = ".repo/local_manifests/pixel.xml"
 custom_default_revision = "oreo-mr1"
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'oreo-mr1')
 custom_dependencies = "aosp.dependencies"
 org_manifest = "pixel-devices"  # leave empty if org is provided in manifest
 org_display = "PixelExperience-Devices"  # needed for displaying
