c153ccec2d5e2a857b71ab14affdf4d28e52406a
Subinsmani <subins.mani@gmail.com>
2019-11-09 07:31:01 +0530

    vendor: Its Time for PixysOS V 3.0.1
    Change-Id: I102a471a1ceb5534ac92cd2d2ba943e3eb475d1c
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PIXYS_MOD_VERSION = v3.0.0
+PIXYS_MOD_VERSION = v3.0.1
 
 ifndef PIXYS_BUILD_TYPE
-PIXYS_BUILD_TYPE := BETA
+PIXYS_BUILD_TYPE := UNOFFICIAL
 
 endif
 
