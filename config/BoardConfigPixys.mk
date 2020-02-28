66b72f1900730717f5718a72b1521cba92e7768f
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-02-28 14:17:36 -0300

    vendor: Add fwk-detect namespace
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I826946e5e7a3e951ceb699ed3b5791813be9ae5b
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- config/BoardConfigPixys.mk
+++ config/BoardConfigPixys.mk
@@ -5,3 +5,8 @@ include vendor/pixys/config/BoardConfigQcom.mk
 endif
 
 include vendor/pixys/config/BoardConfigSoong.mk
+
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
