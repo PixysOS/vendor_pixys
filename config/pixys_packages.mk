f5e946263083c7be4f894ac2a3a27636c533cab6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    Port face unlock feature
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>
    Change-Id: I2a2f9868bd21eff420c5f3f40657443146221dcf

--- config/pixys_packages.mk
+++ config/pixys_packages.mk
@@ -43,6 +43,16 @@ PRODUCT_PACKAGES += \
     wget \
     zip
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.exfat \
