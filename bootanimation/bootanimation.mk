fbe70d36e066f3b7a9a4a3f0a63d3206def04577
Subinsmani <subins.mani@gmail.com>
2020-05-22 17:34:55 +0530

    vendor: Move the Bootanimation to TARGET COPY OUT PRODUCT
    Signed-off-by: Subinsmani <subins.mani@gmail.com>
    Change-Id: I2e169302b35d079fe00e19584906668463673853

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -14,11 +14,11 @@
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_720.zip:system/product/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1080.zip:system/product/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1440.zip:system/prduct/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pixys/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     $(error "Bootanimation: TARGET_BOOT_ANIMATION_RES is undefined")
 endif
