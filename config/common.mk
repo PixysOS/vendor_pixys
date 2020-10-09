9e266352336b81c6b395315e2dddf2ead144b482
aswin7469 <aswinas@pixysos.com>
2020-10-09 03:09:54 +0000

    pixys: disable blurs by default
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I98554b8e09dec6d45e931c01817454fa775c4f24

--- config/common.mk
+++ config/common.mk
@@ -30,7 +30,8 @@ endif
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
