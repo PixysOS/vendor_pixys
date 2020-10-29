AOSiP/platform_vendor_aosip

copy backuptool files to system
For some reason in android11 it fails to copy to $OUT/install, so copy to system as a temporary workaround till the root cause is discovered

Change-Id: I14f8d38436a955f4fe8f491cfe7be28b58a2984e
Signed-off-by: Akhil Narang <me@akhilnarang.dev>

config/common.mk
@@ -33,8 +33,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 
 PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/aosip/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosip/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/aosip/prebuilt/common/bin/backuptool.functions:$(TARGET_COPYT_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/aosip/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)

PixysOS/vendor_pixys

config/common.mk
@@ -46,8 +46,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/pixys/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/pixys/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/pixys/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/pixys/prebuilt/common/bin/backuptool.functions:$(TARGET_COPYT_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/pixys/prebuilt/common/bin/50-pixys.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-pixys.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)