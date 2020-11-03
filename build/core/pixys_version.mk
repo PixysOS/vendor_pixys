576a49462b4f01cbc8c6fec3d6106f179ac328c8
Subinsmani <subins.mani@pixysos.com>
2020-11-03 19:50:57 +0530

    build: Upstream PixysOS Version
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>
    Change-Id: Ic5dbf515fe297a816821f28baa7f68d6cc298fba

--- build/core/pixys_version.mk
+++ build/core/pixys_version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PIXYS_MOD_VERSION = v4.0.1
+PIXYS_MOD_VERSION = v4.0.2
 
 ifndef PIXYS_BUILD_TYPE
 PIXYS_BUILD_TYPE := BETA
