cf96c66f1c54aef7afc8b93f6a493c045ac26e3a
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    pixys: config: Build the textclassifier models
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/pixys_packages.mk
+++ config/pixys_packages.mk
@@ -3,6 +3,13 @@ PRODUCT_PACKAGES += \
     Updater \
     ThemePicker
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+	libtextclassifier_annotator_en_model \
+	libtextclassifier_annotator_universal_model \
+	libtextclassifier_actions_suggestions_universal_model \
+	libtextclassifier_lang_id_model
+
 # Extra tools in Pixys
 PRODUCT_PACKAGES += \
     7z \
