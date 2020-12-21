9000168f5d6866c743d1d7026e9f3c7e498387d2
Subinsmani <subins.mani@pixysos.com>
2020-12-21 14:38:59 +0530

    pixys: Upstream Pixys Version
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>
    Change-Id: I4784f9ff38dbc4ecc07cb22c2b194a855d8443df

--- build/core/pixys_version.mk
+++ build/core/pixys_version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PIXYS_MOD_VERSION = v4.0.2
+PIXYS_MOD_VERSION = v4.0.3
 
 ifndef PIXYS_BUILD_TYPE
 PIXYS_BUILD_TYPE := UNOFFICIAL
