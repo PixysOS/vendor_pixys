2dd4757614259c05b64c961abb4128f2cb40027a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 15:37:23 -0300

    vendor: Add back FaceUnlock support
    Change-Id: I361ce913dfb5f1db574b3cf3cdeeeaaca2338ac1
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/pixys_packages.mk
+++ config/pixys_packages.mk
@@ -80,3 +80,14 @@ endif
 
 # Potato Plugins
 -include packages/apps/Plugins/plugins.mk
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
