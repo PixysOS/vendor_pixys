e8b9adb2812e2ffde21de1ad82ca1db43370cc3c
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-22 12:36:48 -0500

    soong: Add TARGET_LEGACY_HW_DISK_ENCRYPTION conditional
    Change-Id: I2c67fe632d415353273839493a86cdd75bd93b23
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -47,6 +47,7 @@ SOONG_CONFIG_pixysNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += pixysQcomVars
 SOONG_CONFIG_pixysQcomVars += \
+    legacy_hw_disk_encryption \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -66,6 +67,7 @@ SOONG_CONFIG_pixysGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMER
 SOONG_CONFIG_pixysGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_pixysGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_pixysNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_pixysQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_pixysQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_pixysQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_pixysQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
