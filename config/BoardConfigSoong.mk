f03b350214f3f6e255693eabaafec266b817c6c6
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-22 12:36:48 -0500

    soong: Add TARGET_LEGACY_HW_DISK_ENCRYPTION conditional
    Change-Id: I2c67fe632d415353273839493a86cdd75bd93b23

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -47,6 +47,7 @@ SOONG_CONFIG_pixysNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += pixysQcomVars
 SOONG_CONFIG_pixysQcomVars += \
+    legacy_hw_disk_encryption \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -73,6 +74,7 @@ SOONG_CONFIG_pixysQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_P
 SOONG_CONFIG_pixysQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_pixysQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_pixysQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_pixysQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
