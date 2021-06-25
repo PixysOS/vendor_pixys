c53132f3bc0396a59d15ca2135513dfd56e3d392
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-06-25 15:56:40 +0200

    fonts: Redo custom fonts
    * convert to blueprint and bring all AndroidManifest inline
    * included fonts are Google Sans, Inter v3.19, Linotte,
      Manrope v4.504, OnePlus Sans and OnePlus Slate
    * Stallix edit: Switch to using GInter and add Harmony OS Sans
    
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Co-authored-by: Stallix <joey@evolution-x.org>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I1399ba6d6c804089cc2fba38163ae5127ea4d310

--- config/fonts.mk
+++ config/fonts.mk
@@ -2,11 +2,6 @@
 PRODUCT_PACKAGES += \
     ArbutusSlab-Regular.ttf
 
-# google-sans family
-PRODUCT_PACKAGES += \
-    GoogleSans-Italic.ttf \
-    GoogleSans-Regular.ttf
-
 # Lustria family
 PRODUCT_PACKAGES += \
     Lustria-Regular.ttf
@@ -55,5 +50,38 @@ PRODUCT_PACKAGES += \
     Barlow-Bold.ttf \
     Barlow-Medium.ttf
 
+# Customization overlays
+PRODUCT_PACKAGES += \
+    FontGInterOverlay \
+    FontGoogleSansOverlay \
+    FontHarmonySansOverlay \
+    FontLinotteSourceOverlay \
+    FontManropeOverlay \
+    FontNotoSerifSource \
+    FontOnePlusSansOverlay \
+    FontOneplusSlateSourceOverlay
+
+PRODUCT_PACKAGES += \
+    GInterVF-Italic.ttf \
+    GInterVF-Roman.ttf \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf \
+    HarmonyOS-Sans-BlackItalic.ttf \
+    HarmonyOS-Sans-Black.ttf \
+    HarmonyOS-Sans-BoldItalic.ttf \
+    HarmonyOS-Sans-Bold.ttf \
+    HarmonyOS-Sans-Italic.ttf \
+    HarmonyOS-Sans-LightItalic.ttf \
+    HarmonyOS-Sans-Light.ttf \
+    HarmonyOS-Sans-MediumItalic.ttf \
+    HarmonyOS-Sans-Medium.ttf \
+    HarmonyOS-Sans-Regular.ttf \
+    HarmonyOS-Sans-ThinItalic.ttf \
+    HarmonyOS-Sans-Thin.ttf \
+    Lexend-VF.ttf \
+    Manrope-VF.ttf \
+
+# Register vendor fonts
 PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/pixys/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/pixys/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
