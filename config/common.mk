e1e82f906b88caf983aacbc2bac182deb9f4b28d
jhenrique09 <jhsv09@gmail.com>
2021-10-11 13:43:05 -0300

    vendor: import fonts from SPB5.210812.002
    Change-Id: I2913ab4271a9f591d002000405bf501aca8846cf
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -97,6 +97,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Bootanimation
 $(call inherit-product, vendor/pixys/bootanimation/bootanimation.mk)
 
+# Fonts
+$(call inherit-product, vendor/pixys/config/fonts.mk)
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
