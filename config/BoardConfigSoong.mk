76c026eb26746ac74a670361b265805030704ba7
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-11-09 19:53:05 +0200

    vendor: soong: Add camera_needs_client_info_lib
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: If44d09bacdf3a676ac2bf6b56021fe77983cb62b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += pixysGlobalVars
 SOONG_CONFIG_pixysGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -66,6 +67,7 @@ SOONG_CONFIG_pixysQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_pixysGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_pixysGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_pixysGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_pixysGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
