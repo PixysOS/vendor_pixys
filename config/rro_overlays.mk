10a5cded727355e914258835e6f31ba5e01b99aa
aswin7469 <aswinas@pixysos.com>
2022-02-19 22:16:57 +0530

    vendor: cleanup Pixel rro_overlays
    * ship prebuilt ones with gapps
    * re organise a bit
    
    Change-Id: I9b38c05bb3b32f53b3785dbbd24768d71e697244
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -1,36 +1,6 @@
 # RRO Overlays
 PRODUCT_PACKAGES += \
-    PixelDocumentsUIGoogleOverlay \
-    BuiltInPrintServiceOverlay \
     CustomPixelLauncherOverlay \
-    GooglePermissionControllerOverlay \
-    PixelConfigOverlayCommon \
-    TeleServiceOverlay \
-    CaptivePortalLoginOverlay \
-    GoogleWebViewOverlay \
-    TelecomOverlay \
-    CellBroadcastServiceOverlay \
-    SettingsGoogleOverlay \
-    SettingsGoogleOverlayPixel2021 \
-    TelephonyProviderOverlay \
-    ContactsProviderOverlay \
-    PixelConfigOverlay2018 \
-    SettingsProviderOverlay \
-    TraceurOverlay \
-    PixelConfigOverlay2019 \
-    PixelConfigOverlay2019Midyear \
-    PixelConfigOverlay2021 \
-    PixelSetupWizardOverlay \
-    PixelSetupWizardOverlay2019 \
-    PixelSetupWizardOverlayActiveEdge \
-    PixelFwResOverlay \
-    GoogleConfigOverlay \
-    SystemUIGoogleOverlay \
     GesturalNavigationOverlayLong \
     GesturalNavigationOverlayMedium \
-   NavigationBarNoHintOverlay
-
-ifeq ($(TARGET_INCLUDE_LIVE_WALLPAPERS),true)
-PRODUCT_PACKAGES += \
-    PixelLiveWallpapersOverlay
-endif
+    NavigationBarNoHintOverlay
