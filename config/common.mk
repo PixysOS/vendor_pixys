Project-Awaken/android_vendor_awaken

vendor: Add game overlay sysconfig
Change-Id: Ie5b46b86125f1fe601bce93a24256607c7ea28d7
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -94,6 +94,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Fonts
 include vendor/awaken/config/fonts.mk
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/awaken/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Gapps
 ifeq ($(USE_GAPPS),true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)

PixysOS/vendor_pixys

Signed-off-by: aswin7469 <aswinas@pixysos.com>

config/common.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.wm.enable_remote_keyguard_animation=0
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/pixys/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Log privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log