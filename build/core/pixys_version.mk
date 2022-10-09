312b892957777910e35bd6c6ecd5d1e33b57901a
aswin7469 <aswinas@pixysos.com>
2022-10-09 13:53:24 +0530

    Revert "vendor: Add PIXYS_SECURITY_PATCH prop"
    This reverts commit 51c47f06ce34986b08da48a469cf601f8588c162.
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I5ab8f14277bfd688ad5d2814bb0df9f90b23ee3e

--- build/core/pixys_version.mk
+++ build/core/pixys_version.mk
@@ -15,9 +15,6 @@
 PIXYS_MOD_VERSION = v5.1.5
 PIXYS_PLATFORM_RELEASE_OR_CODENAME := 12L
 
-# Custom security patch
-PIXYS_SECURITY_PATCH := 2022-08-05
-
 ifndef PIXYS_BUILD_TYPE
 PIXYS_BUILD_TYPE := UNOFFICIAL
 
@@ -58,7 +55,6 @@ PRODUCT_GENERIC_PROPERTIES += \
   ro.pixys.releasetype=$(PIXYS_BUILD_TYPE) \
   ro.modversion=$(PIXYS_MOD_VERSION) \
   ro.pixys.build.date=$(PIXYS_BUILD_DATETIME)\
-  ro.pixys.build_security_patch=$(PIXYS_SECURITY_PATCH) \
   ro.pixys.display.version=$(PIXYS_DISPLAY_VERSION) \
   ro.pixys.fingerprint=$(PIXYS_FINGERPRINT)\
   ro.pixys.edition=$(PIXYS_EDITION)\
