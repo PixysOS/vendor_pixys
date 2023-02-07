efb7600d08f6034afe7d00532ec400c37f8b82cd
Azure-Helper <azurehelper219+1@gmail.com>
2023-02-07 17:24:11 +0000

    pixys: Update Pixys date & time variables - Stop using UNIX epoch time format
    Change-Id: I35a612bf68adf1c848ca05a74d4097672fde8cae

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -63,7 +63,7 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET) \
 	@echo -e ${CL_CYN}"MD5            : "${CL_MAG}" $(shell cat $(PIXYS_TARGET_PACKAGE).md5sum | awk '{print $$1}')"${CL_RST}
 	@echo -e ${CL_CYN}"Size           : "${CL_MAG}" $(shell du -hs $(PIXYS_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
 	@echo -e ${CL_CYN}"Size(in bytes) : "${CL_MAG}" $(shell wc -c $(PIXYS_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
-	@echo -e ${CL_CYN}"DateTime       : "${CL_MAG} $(PIXYS_BUILD_DATETIME)${CL_RST}
+        @echo -e ${CL_CYN}"DateTime       : "${CL_MAG} $(PIXYS_BUILD_DATE)-$(PIXYS_BUILD_DATE)${CL_RST}
 	@echo -e ${CL_CYN}"Build Type     : "${CL_MAG} $(PIXYS_BUILD_TYPE)${CL_RST}
 	@echo -e ${CL_CYN}"==========================================================================="${CL_RST}
 	@echo -e ""
