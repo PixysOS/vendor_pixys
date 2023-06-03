bcf125792800ca1895901a96dc035abfff352a4c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-03 14:20:21 +0530

    common: Switch to AOSPA's face unlock implementation
    Change-Id: Ib1042d40b3d44fbde6eda8d6635fcb720e4b8d4b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -166,12 +166,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 # faceunlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= true
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
-    FaceUnlockService
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service.enabled=true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
