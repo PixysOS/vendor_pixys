b574ba598790068b940452ea732aa24fe92d33a3
jhenrique09 <jhsv09@gmail.com>
2023-07-11 20:27:13 -0300

    vendor: Add TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT
    Change-Id: I0b5ded178ec35f87f768509e497ecce4926d4336
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -39,6 +39,7 @@ SOONG_CONFIG_pixysGlobalVars += \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
+    camera_uses_newer_hidl_override_format \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
@@ -92,6 +93,7 @@ SOONG_CONFIG_pixysQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DE
 SOONG_CONFIG_pixysGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_pixysGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_pixysGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_pixysGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
