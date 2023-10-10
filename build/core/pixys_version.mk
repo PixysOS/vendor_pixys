054329d47673f43ef70f24a7508e5a3223b52c6b
aswin7469 <aswinas@pixysos.com>
2023-10-10 11:59:25 +0530

    build: Hello UpsideDownCake
    Change-Id: I81f4af3dcace0e06694cae6bf5a51affd36d88f1
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- build/core/pixys_version.mk
+++ build/core/pixys_version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PIXYS_MOD_VERSION = v6.3.4
+PIXYS_MOD_VERSION = v7.0.0
 
 ifndef PIXYS_BUILD_TYPE
 PIXYS_BUILD_TYPE := UNOFFICIAL
