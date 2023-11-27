b9f4d704581f0966b638c3846da9d5169329d5e1
Luofan Chen <clfbbn@gmail.com>
2023-11-27 22:30:51 +0800

    Hookup SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC
    Change-Id: Icb4b8aebd8042e9f83bacddee1e7949cf373226d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -43,6 +43,7 @@ SOONG_CONFIG_pixysGlobalVars += \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
     camera_uses_newer_hidl_override_format \
+    sdmcore_has_is_display_hw_available_func \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -110,6 +111,7 @@ SOONG_CONFIG_pixysGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_
 SOONG_CONFIG_pixysQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_pixysGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 SOONG_CONFIG_pixysGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
+SOONG_CONFIG_pixysGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
@@ -126,6 +128,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE ?= true
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
 TARGET_QTI_VIBRATOR_EFFECT_LIB ?= libqtivibratoreffect
+TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC ?= true
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
