01b6eabf70dab39c2ebe0ca333c503d59e8dbf00
aswin7469 <aswinas@pixysos.com>
2023-12-03 15:20:59 +0530

    onfig: Move to AOSPA face sense impl
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -165,12 +165,15 @@ endif
 TARGET_SUPPORTS_GOOGLE_BATTERY ?= false
 
 # faceunlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= false
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+
 PRODUCT_PACKAGES += \
-    FaceUnlockService
+    ParanoidSense
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    ro.face.sense_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
